48ed52acf2c606b0d107e81af7ab524b1a958442
Aaron Zhang <aaron.z@covergenius.com>
2021-08-25 00:20:47 +1000

    by default export django env variables

--- ragdoll/django.py
+++ ragdoll/django.py
@@ -5,9 +5,11 @@ from ragdoll.env import EnvSetting
 
 
 class DjangoSettingMixin:
+    default_export = True
+
     @classmethod
     def configure_entry(cls, entry: base.BaseEntry, name: str, value: str):
-        if entry.extra_kwargs.get("export"):
+        if entry.extra_kwargs.get("export", cls.default_export):
             setattr(sys.modules[cls.__module__], name, value)
 
 
--- tests/test_django/test_django.py
+++ tests/test_django/test_django.py
@@ -3,10 +3,24 @@ from ragdoll.env import StrEnv
 
 
 def test_django_env_setting_export(monkeypatch):
+    global FOO
     monkeypatch.setenv("FOO", "BAR")
 
     class MyDjangoEnvSetting(DjangoEnvSetting):
-        FOO = StrEnv(export=True)
+        FOO = StrEnv()  # export=True
 
     assert "FOO" in globals()
-    assert FOO == "BAR"  # noqa: F821
+    assert FOO == "BAR"
+    assert MyDjangoEnvSetting.FOO == "BAR"
+    del FOO
+
+
+def test_django_env_setting_no_export(monkeypatch):
+    global FOO
+    monkeypatch.setenv("FOO", "BAR")
+
+    class MyDjangoEnvSetting(DjangoEnvSetting):
+        FOO = StrEnv(export=False)
+
+    assert "FOO" not in globals()
+    assert MyDjangoEnvSetting.FOO == "BAR"
